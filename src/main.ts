f709e69029610b7e82e8e17ef40abff08febdc29
Hakob29 <hakobyan29001@gmail.com>
2023-05-24 23:22:34 +0400

    First commit, server start

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  const PORT = config.get("PORT");
+  const HOST = config.get("HOST");
+  await app.listen(PORT || 3001, () => {
+    console.log("Server has been connected on " + HOST + PORT)
+  });
+}
+bootstrap();
