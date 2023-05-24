b6338b1b05bd7f55e894d1ce2c77928a97bedf64
Hakob29 <hakobyan29001@gmail.com>
2023-05-25 00:11:06 +0400

    Docker-compose, DB connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from 'ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(AppDataSource.options)
+  ],
   controllers: [],
   providers: [],
 })
